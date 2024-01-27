59ae1d45d00358bea57112cf4d920cc4907890e4
Bogdan <milosbogdan262@yahoo.com>
2024-01-27 15:00:22 +0200

    added reticle

--- Assets/Scripts/GrabObjects.cs
+++ Assets/Scripts/GrabObjects.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class GrabObjects : MonoBehaviour
 {
-    Transform hand;
+    public Transform hand;
+    GameObject pickedItem;
+    public bool isPicked;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isPicked = false;
     }
 
     // Update is called once per frame
@@ -16,16 +18,32 @@ public class GrabObjects : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(mouseRay, out hit))
+            if (isPicked == false)
             {
-                if (hit.collider.tag=="interactable")
+                Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(mouseRay, out hit))
                 {
-                    GameObject pickedItem = hit.collider.gameObject;
-                    pickedItem.GetComponent<Rigidbody>().isKinematic = true;
-                    pickedItem.transform.position = hand.position;
+
+                    if (hit.collider.tag == "Interactable")
+                    {
+                        pickedItem = hit.collider.gameObject;
+                        pickedItem.GetComponent<Rigidbody>().isKinematic = true;
+                        pickedItem.transform.position = hand.position;
+                        pickedItem.transform.rotation = Quaternion.Euler(-90, 0, 180 );
+                        pickedItem.transform.parent = hand;
+                        pickedItem.GetComponent<Collider>().enabled = false;
+                        isPicked = true;
+                    }
                 }
+            }
+            else
+            if (isPicked == true)
+            {
+                pickedItem.GetComponent<Rigidbody>().isKinematic = false;
+                pickedItem.transform.parent = null;
+                isPicked = false;
+                pickedItem.GetComponent<Collider>().enabled = true;
             }
         }
     }
