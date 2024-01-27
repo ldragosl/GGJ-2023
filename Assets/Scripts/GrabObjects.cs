ecf216733879448d4fae0a4a32fe18e9915482ae
Bogdan <milosbogdan262@yahoo.com>
2024-01-27 15:16:06 +0200

    added UI

--- Assets/Scripts/GrabObjects.cs
+++ Assets/Scripts/GrabObjects.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GrabObjects : MonoBehaviour
 {
     public Transform hand;
     GameObject pickedItem;
     public bool isPicked;
+    public int range = 4;
+    public TMP_Text pickUpText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,35 +20,37 @@ public class GrabObjects : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (isPicked == false)
         {
-            if (isPicked == false)
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(mouseRay, out hit))
             {
-                Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if (Physics.Raycast(mouseRay, out hit))
+                if (hit.collider.tag == "Interactable" && Vector3.Distance(hit.collider.gameObject.transform.position, gameObject.transform.position) <= range)
                 {
-
-                    if (hit.collider.tag == "Interactable")
+                    Debug.Log("in range up");
+                    
+                    if (Input.GetMouseButtonDown(0))
                     {
                         pickedItem = hit.collider.gameObject;
                         pickedItem.GetComponent<Rigidbody>().isKinematic = true;
                         pickedItem.transform.position = hand.position;
-                        pickedItem.transform.rotation = Quaternion.Euler(-90, 0, 180 );
+                        pickedItem.transform.rotation = Quaternion.Euler(-90, 0, 180);
                         pickedItem.transform.parent = hand;
                         pickedItem.GetComponent<Collider>().enabled = false;
                         isPicked = true;
+                        pickUpText.enabled = false;
                     }
                 }
             }
-            else
-            if (isPicked == true)
-            {
-                pickedItem.GetComponent<Rigidbody>().isKinematic = false;
-                pickedItem.transform.parent = null;
-                isPicked = false;
-                pickedItem.GetComponent<Collider>().enabled = true;
-            }
+        }
+        else
+        if (isPicked == true)
+        {
+            pickedItem.GetComponent<Rigidbody>().isKinematic = false;
+            pickedItem.transform.parent = null;
+            isPicked = false;
+            pickedItem.GetComponent<Collider>().enabled = true;
         }
     }
 }
