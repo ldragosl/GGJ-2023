5078564d4dc20feff074a364f47c37b73108de13
Dan-The-Assassin <dantheassassin1702@gmail.com>
2024-01-28 12:05:24 +0200

    Noble navmesh
    Added noble script, navmesh and noble positions.

---
+++ Assets/Scripts/NobleBehavior.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NobleBehavior : MonoBehaviour
+{
+    private const float _waitMax = 5.0f;
+    private float _waitTime;
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
