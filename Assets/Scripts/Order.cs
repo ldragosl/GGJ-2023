548d3a893b0f8c9e2020b1795125012ec214a42f
Dan-The-Assassin <dantheassassin1702@gmail.com>
2024-01-28 14:37:42 +0200

    Working Score

--- Assets/Scripts/Order.cs
+++ Assets/Scripts/Order.cs
@@ -19,6 +19,7 @@ public class Order : MonoBehaviour
     public virtual void fulfill()
     {
         fulfilled = true;
+        GameObject.Find("Score").GetComponent<ScoreController>().addScore(1);
         orderPlacer.AdvanceState();
     }
 
