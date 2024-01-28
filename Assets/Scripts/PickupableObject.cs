067a542b9ef924eef09e08de92804045fb5f0fd9
dragos bogdan tudor <dragosbogdan001@hotmail.com>
2024-01-28 11:04:50 +0200

    added meat generator

--- Assets/Scripts/PickupableObject.cs
+++ Assets/Scripts/PickupableObject.cs
@@ -7,13 +7,25 @@ public class PickupableObject : MonoBehaviour
     public Transform hand;
     GrabObjects grabObject;
     public bool isPicked = false;
+    public bool isGenerator = false;
     void Start()
     {
         grabObject = GrabObjects.singleton;
     }
 
+    void GenerateNew()
+    {
+        if (isGenerator)
+        {
+            GameObject inst = Instantiate(gameObject, transform.position, transform.rotation);
+            isGenerator = false;
+        }
+    }
+
     public virtual void OnPickup()
     {
+        GenerateNew();
+
         GetComponent<Rigidbody>().isKinematic = true;
         transform.position = hand.position;
         transform.rotation = Quaternion.Euler(-90, 0, 180);
